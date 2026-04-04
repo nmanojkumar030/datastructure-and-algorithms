10139e37f92d1a54be519baf6ce652354c965c9c
Manoj Kumar N <nmanojkumar030@gmail.com>
2026-04-04 09:22:08 +0530

    Adding additional problems

---
+++ src/main/java/pattern/sorting/MaximumProductOfThreeNumbers.java
@@ -0,0 +1,23 @@
+package pattern.sorting;
+
+import java.util.Arrays;
+
+public class MaximumProductOfThreeNumbers {
+
+    public static void main(String[] args) {
+        MaximumProductOfThreeNumbers maximumProductOfThreeNumbers = new MaximumProductOfThreeNumbers();
+        int result = maximumProductOfThreeNumbers.maximumProduct(new int[]{1, 2, 3, 4});
+        System.out.println("maximum product of three numbers is " + result);
+
+        int result2 = maximumProductOfThreeNumbers.maximumProduct(new int[]{-1, -2, 3, 4});
+        System.out.println("maximum product of three numbers is " + result2);
+    }
+
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int max1 = nums[nums.length - 1] * nums[nums.length - 2] * nums[nums.length - 3];
+        int max2 = nums[nums.length - 1] * nums[0] * nums[1];
+
+        return Math.max(max1, max2);
+    }
+}
